3fc867ecbb2f1d8ff36d22dfe7ebfcc8bacf4bcd
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-07-10 13:54:36 +0200

    move amount_for method from Customer to Rental

--- refactor.rb
+++ refactor.rb
@@ -18,6 +18,22 @@ class Rental
   def initialize(movie, days_rented)
     @movie, @days_rented = movie, days_rented
   end
+
+  def charge
+    result = 0
+    case movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end 
+
 end
 
 class Customer
@@ -57,19 +73,9 @@ class Customer
   end
 
   def amount_for(rental)
-    result = 0
-    case rental.movie.price_code
-    when Movie::REGULAR
-      result += 2
-      result += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      result += rental.days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-    end
-    result
-  end 
+    rental.charge
+  end
+
 end
 
 
