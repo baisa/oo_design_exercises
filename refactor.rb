2f1554f7e5be045f645fffe4da9e403bff37d044
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-07-10 16:17:54 +0200

    replacing total_amount variable

--- refactor.rb
+++ refactor.rb
@@ -54,7 +54,7 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
+    frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
@@ -65,11 +65,20 @@ class Customer
     end
 
     #add footer lines
-    result += "Amount owed is #{total_amount}\n"
+    result += "Amount owed is #{total_charge}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
   end
 
+  private
+
+  def total_charge
+    result = 0
+    @rentals.each do |element|
+      result += element.charge
+    end
+    result
+  end
 
 end
 
