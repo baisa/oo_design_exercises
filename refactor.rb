ecd1beb1f614eced8eef3783086e93589f4578ee
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-07-10 13:43:08 +0200

    extracting amount_for method

--- refactor.rb
+++ refactor.rb
@@ -38,18 +38,7 @@ class Customer
     result = "Rental Record for #{@name}\n"
 
     @rentals.each do |element|
-      this_amount = 0
-
-      case element.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += element.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
-      end
+      this_amount = amount_for(element)
 
       frequent_renter_points += 1
       if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
@@ -67,6 +56,19 @@ class Customer
     result
   end
 
+  def amount_for(element)
+    this_amount = 0
+    case element.movie.price_code
+    when Movie::REGULAR
+      this_amount += 2
+      this_amount += (element.days_rented - 2) * 1.5 if element.days_rented > 2
+    when Movie::NEW_RELEASE
+      this_amount += element.days_rented * 3
+    when Movie::CHILDRENS
+      this_amount += 1.5
+      this_amount += (element.days_rented - 3) * 1.5 if element.days_rented > 3
+    end
+  end 
 end
 
 
